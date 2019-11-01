5b8b2879ff6044b1155d8c2ecb9fb559327ed90a
Ray <raymundosaraiva@hotmail.com>
2019-11-01 17:13:29 -0300

    Materialize Project

---
+++ pokebattle/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+from django.conf.urls import url
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('', views.index, name='battle'),
+    path('', views.index, name='mypokemon'),
+]
