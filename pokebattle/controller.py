65d2b9b7c50448a9568f0911df18fe045737f770
Ray <raymundosaraiva@hotmail.com>
2019-11-02 22:21:06 -0300

    CHange from IP to MAC Address

--- pokebattle/controller.py
+++ pokebattle/controller.py
@@ -1,19 +1,18 @@
 import datetime
+import uuid
 
 from .models import *
 
 
 def login(request):
-    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-    if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[0]
-    else:
-        ip = request.META.get('REMOTE_ADDR')
+    mac = hex(uuid.getnode())
+    if not mac:
+        return None
     try:
-        trainer = Trainer.objects.get(ip=ip)
+        trainer = Trainer.objects.get(mac=mac)
         print(f'Login from IP: {ip} at {datetime.datetime.now()}')
     except Trainer.DoesNotExist:
-        trainer = Trainer(ip=ip)
+        trainer = Trainer(mac=mac)
         trainer.save()
         print('Creating new user!')
     return trainer
--- pokebattle/models.py
+++ pokebattle/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Trainer(models.Model):
-    ip = models.GenericIPAddressField(primary_key=True, unique=True)
+    mac = models.CharField(primary_key=True, unique=True, max_length=40)
     nickname = models.CharField(max_length=20, default='Anonymous')
     img = models.ImageField(blank=True, null=True)
     date_created = models.DateTimeField(auto_now_add=True)
