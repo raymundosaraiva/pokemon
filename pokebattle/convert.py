e10852059f38679b83ed4f5ad638d07d381487dd
Edgar Tamio Hirama <edgar.hirama@arctouch.com>
2019-11-06 22:27:55 -0300

    unify pokemon json files

--- pokebattle/convert.py
+++ pokebattle/convert.py
@@ -1,16 +1,25 @@
 import json
 
 jsonPath = './static/pokebattle/json/'
-with open(jsonPath + 'pokemon_stats.json') as pokemon_stats, open(jsonPath + 'pokemon_types.json') as pokemon_types: 
-	stats = json.load(pokemon_stats)
-	types = json.load(pokemon_types)
-	for s, t in zip(stats, types):
-		pokemon_id = s['pokemon_id']
-		name = s['pokemon_name']
-		pokemon_type = t['type']
-		attack = s['base_attack']
-		defense = s['base_defense']
-		stamina = s['base_stamina']
+with open(jsonPath + 'pokemon_stats.json') as pokemon_stats, open(jsonPath + 'pokemon_types.json') as pokemon_types:
+    stats = json.load(pokemon_stats)
+    types = json.load(pokemon_types)
+    poke_list = []
+    for s, t in zip(stats, types):
+        form = s.get('form')
 
+        if not form or (form and form == "Normal"):
+            pokemon = {
+                "pokemon_id": s['pokemon_id'],
+                "name": s['pokemon_name'],
+                "pokemon_type": t['type'],
+                "attack": s['base_attack'],
+                "defense": s['base_defense'],
+                "stamina": s['base_stamina']
+            }
+            poke_list.append(pokemon)
 
-		
+    print(len(poke_list))
+
+with open(jsonPath + "poke_file.json", "w") as write_file:
+    json.dump(poke_list, write_file, indent=4)
