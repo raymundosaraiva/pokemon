c8e53cbcdccf480b9bf76d0c6f929445201ee319
Edgar Tamio Hirama <edgar.hirama@arctouch.com>
2019-11-09 00:28:46 -0300

    trying to create pokemon models

--- pokebattle/convert.py
+++ pokebattle/convert.py
@@ -1,25 +1,28 @@
 import json
+from .models import Pokemon
 
 jsonPath = './static/pokebattle/json/'
 with open(jsonPath + 'pokemon_stats.json') as pokemon_stats, open(jsonPath + 'pokemon_types.json') as pokemon_types:
-    stats = json.load(pokemon_stats)
-    types = json.load(pokemon_types)
-    poke_list = []
-    for s, t in zip(stats, types):
-        form = s.get('form')
+	stats = json.load(pokemon_stats)
+	types = json.load(pokemon_types)
+	poke_list = []
+	for s, t in zip(stats, types):
+		form = s.get('form')
 
-        if not form or (form and form == "Normal"):
-            pokemon = {
-                "pokemon_id": s['pokemon_id'],
-                "name": s['pokemon_name'],
-                "pokemon_type": t['type'],
-                "attack": s['base_attack'],
-                "defense": s['base_defense'],
-                "stamina": s['base_stamina']
-            }
-            poke_list.append(pokemon)
-
-    print(len(poke_list))
+		if not form or (form and form == "Normal"):
+			pokemon = {
+				"pokemon_id": s['pokemon_id'],
+				"name": s['pokemon_name'],
+				"pokemon_type": t['type'],
+				"attack": s['base_attack'],
+				"defense": s['base_defense'],
+				"stamina": s['base_stamina']
+			}
+			poke_list.append(pokemon)
 
 with open(jsonPath + "poke_file.json", "w") as write_file:
-    json.dump(poke_list, write_file, indent=4)
+	json.dump(poke_list, write_file, indent=4)
+
+for pokemon in poke_list:
+	new_pokemon = Pokemon(**pokemon)
+	new_pokemon.save()
