bed771c30928a8793fc1412615a48be91b36c790
Edgar Tamio Hirama <edgar.hirama@arctouch.com>
2019-11-28 00:57:14 -0300

    get trainer from context in Stats view

--- pokebattle/views.py
+++ pokebattle/views.py
@@ -31,10 +31,10 @@ def pokemon(request, pokemon_id):
 
 def stats(request):
     context = login(request)
-    trainer_id = request.session[USER_LOGIN]
-    games = Game.objects.filter(trainer_id=trainer_id, status=2)
-    gamesWon = Game.objects.filter(trainer_id=trainer_id, status=2, final_result=3)
-    gamesLost = Game.objects.filter(trainer_id=trainer_id, status=2, final_result=1)
+    trainer = context.get('trainer')
+    games = Game.objects.filter(trainer_id=trainer.id, status=2)
+    gamesWon = Game.objects.filter(trainer_id=trainer.id, status=2, final_result=3)
+    gamesLost = Game.objects.filter(trainer_id=trainer.id, status=2, final_result=1)
     context.update({'games': games,
                     'gamesWon': gamesWon,
                     'gamesLost': gamesLost})
