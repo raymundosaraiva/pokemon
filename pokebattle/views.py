debc04aeba42ed1db0158db0539e81a23344869b
Ray <raymundosaraiva@hotmail.com>
2019-11-28 00:39:53 -0300

    Spacing and validations

--- pokebattle/views.py
+++ pokebattle/views.py
@@ -18,9 +18,9 @@ def my_pokemon(request):
     trainer = context.get('trainer')
     pokemon = [get_pokemon_and_img_url(pokemon) for pokemon in trainer.pokemon_collection.all()]
     context.update({'my_pokemon': pokemon,
-                    'colsize': int(12 / len(pokemon)) if len(pokemon) > 0 else 12,
                     'pokemon_count': len(pokemon)})
     return render(request, 'pokebattle/my_pokemon.html', context)
+
 
 def pokemon(request, pokemon_id):
     context = login(request)
